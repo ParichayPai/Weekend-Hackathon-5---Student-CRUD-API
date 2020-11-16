3a6bf0a07397fce5888413b42b5a35987bf996d8
ParichayPai <parichaypai393@gmail.com>
2020-11-16 14:05:59 +0530

    fin

--- src/index.js
+++ src/index.js
@@ -41,7 +41,7 @@ app.post("/api/student", (req, res) => {
         division:division
     }
     doc.push(obj);
-    res.send(obj);
+    res.send(obj.id);
 });
 
 app.put("/api/student/:id", (req, res) => {
